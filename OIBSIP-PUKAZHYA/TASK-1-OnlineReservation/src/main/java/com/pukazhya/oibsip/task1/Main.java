d574fe99db8bf21289efc49e61cd1e8e001ce76c
PUKAZHYA <pukazhyap07@gmail.com>
2025-11-02 17:42:58 +0530

    Initial commit - OIBSIP internship tasks

---
+++ OIBSIP-PUKAZHYA/TASK-1-OnlineReservation/src/main/java/com/pukazhya/oibsip/task1/Main.java
@@ -0,0 +1,8 @@
+﻿package com.pukazhya.oibsip.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TASK-1 Online Reservation: Hello from Main!");
+        // Replace this with your actual code or call your classes/methods here.
+    }
+}
